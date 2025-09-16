bf723eee1f3d1c05e84192d2000c31ac24a24f79
saad-naveed-brainx <saad.naveed@brainxtech.com>
2025-09-16 12:31:17 +0500

    added output directory in vercel.json

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    
+
     origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
