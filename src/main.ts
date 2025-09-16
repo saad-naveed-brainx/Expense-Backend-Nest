4d5832f30e9c848bec7b938bf7222d181b6291d2
saad-co <bitf21m041@pucit.edu.pk>
2025-09-16 12:18:35 +0500

    allowed all origins to request to server

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
