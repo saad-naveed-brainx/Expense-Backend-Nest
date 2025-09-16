33c33337fb7adbd3b6dd67afa08cfaebf5ad0574
saad-naveed-brainx <saad.naveed@brainxtech.com>
2025-09-16 12:36:26 +0500

    modified the main.ts file

--- src/main.ts
+++ src/main.ts
@@ -4,26 +4,33 @@ import { AllExceptionsFilter } from './global.exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
 
+  app.enableCors({
     origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
-  app.useGlobalPipes(new ValidationPipe({
-    forbidNonWhitelisted: true,
-    transform: true,
-    transformOptions: { enableImplicitConversion: true },
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
 
   app.use(cookieParser());
-
   app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(process.env.PORT ?? 3000);
+
+  return app.init();
 }
-bootstrap();
+
+const bootstrapPromise = bootstrap();
+export default async function handler(req: any, res: any) {
+  const app = await bootstrapPromise;
+  const server = app.getHttpAdapter().getInstance();
+  return server(req, res);
+}
