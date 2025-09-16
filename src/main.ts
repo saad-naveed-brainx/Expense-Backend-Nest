9279f5798f80da4b2c675160ff630b57438aa0e7
saad-naveed-brainx <saad.naveed@brainxtech.com>
2025-09-16 13:51:58 +0500

    added vercel.json

--- src/main.ts
+++ src/main.ts
@@ -4,33 +4,26 @@ import { AllExceptionsFilter } from './global.exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.enableCors({
+
     origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      forbidNonWhitelisted: true,
-      transform: true,
-      transformOptions: { enableImplicitConversion: true },
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe({
+    forbidNonWhitelisted: true,
+    transform: true,
+    transformOptions: { enableImplicitConversion: true },
+  }));
 
   app.use(cookieParser());
+
   app.useGlobalFilters(new AllExceptionsFilter());
-
-  return app.init();
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-const bootstrapPromise = bootstrap();
-export default async function handler(req: any, res: any) {
-  const app = await bootstrapPromise;
-  const server = app.getHttpAdapter().getInstance();
-  return server(req, res);
-}
+bootstrap();
